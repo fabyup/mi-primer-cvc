e6223721d6777f832f596ec5b6ef61bc2737df03
FABIANA BLASCO <93226475+fabyup@users.noreply.github.com>
2023-02-23 19:06:52 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,66 @@
+   
+          
+     const switcher = document.querySelector('.btn');
+ 
+    switcher.addEventListener('click',function() {
+    document.body.classList.toggle('dark-theme');
+
+   
+     var className = document.body.className;
+    if(className == "light-theme"){
+   
+     this.textContent = "CLICK AQUI";
+       }
+    else{
+        this.textContent = "CLICK AQUI";
+    }
+    });   
+   
+  
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
